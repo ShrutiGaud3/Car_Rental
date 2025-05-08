b7a220fbed9f9e8a03306edf8218f799a39862c1
ShrutiGaud3 <shrutigaud3@gmail.com>
2025-05-08 16:26:13 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss(),],
-  // server :{
-  //   proxy :{
-  //     '/api' :{
-  //       target : "https://car-rental-app-5d25.onrender.com",
-  //       changeOrigin : true,
-  //       secure : false
-  //     }
-  //   }
-  // }
 
  
 })
